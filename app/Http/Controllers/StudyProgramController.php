98c164ae4bb225bf9c579d4d14c21d49bd7a5bc7
Ambara Yasa <81506775+ambarayasa@users.noreply.github.com>
2026-01-12 20:59:18 +0800

    feat(studyprogram): create migration #3

---
+++ app/Http/Controllers/StudyProgramController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreStudyProgramRequest;
+use App\Http\Requests\UpdateStudyProgramRequest;
+use App\Models\StudyProgram;
+
+class StudyProgramController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreStudyProgramRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateStudyProgramRequest $request, StudyProgram $studyProgram)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(StudyProgram $studyProgram)
+    {
+        //
+    }
+}
