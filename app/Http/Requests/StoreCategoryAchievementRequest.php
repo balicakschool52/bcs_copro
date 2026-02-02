3a29d976a03dfd587fdc328c2e1e20e8ef0618c9
Ambara Yasa <81506775+ambarayasa@users.noreply.github.com>
2026-02-02 12:09:39 +0800

    feat(categoryachievement): crud category achievement #6

--- app/Http/Requests/StoreCategoryAchievementRequest.php
+++ app/Http/Requests/StoreCategoryAchievementRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreCategoryAchievementRequest extends FormRequest
 {
@@ -11,7 +12,7 @@ class StoreCategoryAchievementRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +23,7 @@ class StoreCategoryAchievementRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => ['required', 'string', Rule::unique('category_achievements')->whereNull('deleted_at')],
         ];
     }
 }
