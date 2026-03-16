b277f0305283bf202643985906781193498b7fd0
Cahya Wibawa <74125680+cahyawibawa12@users.noreply.github.com>
2026-03-16 14:00:09 +0800

    fix(user): logic in create user #0

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use App\Filament\Resources\Users\Support\UserRoleProfileSync;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
@@ -11,45 +12,11 @@ class CreateUser extends CreateRecord
 
     protected function afterCreate(): void
     {
-        $data = $this->form->getState();
-        $user = $this->record;
-
-        if ($data['role'] === 'student') {
-            $user->student()->create([
-                'nim' => $data['nim'] ?? null,
-                'study_program_id' => $data['study_program_id'] ?? null,
-            ]);
-        }
-
-        if ($data['role'] === 'lecture') {
-            $user->lecture()->create([
-                'nip' => $data['nip'] ?? null,
-                'study_program_id' => $data['study_program_id'] ?? null,
-                'description' => $data['description'] ?? null,
-            ]);
-        }
+        UserRoleProfileSync::sync($this->record, $this->form->getState());
     }
 
     protected function mutateFormDataBeforeCreate(array $data): array
     {
-        unset(
-            $data['nim'],
-            $data['study_program_id'],
-            $data['nip'],
-            $data['description'],
-        );
-
-        return $data;
-    }
-
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        unset(
-            $data['nim'],
-            $data['study_program_id'],
-            $data['nip'],
-            $data['description'],
-        );
-        return $data;
+        return UserRoleProfileSync::userPayload($data);
     }
 }
