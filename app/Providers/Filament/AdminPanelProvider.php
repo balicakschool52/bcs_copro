a05e49b48a823e1fcab5d4c1b272c91404280ac5
Cahya Wibawa <74125680+cahyawibawa12@users.noreply.github.com>
2026-04-02 17:00:25 +0800

    feat(header): add button in header #0

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -11,7 +11,7 @@ use Filament\Pages\Dashboard;
 use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
-use Filament\Support\Icons\Heroicon;
+use Filament\View\PanelsRenderHook;
 use Filament\Widgets\AccountWidget;
 use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
@@ -33,12 +33,17 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->globalSearch(false)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
             ->pages([
                 Dashboard::class,
             ])
             ->databaseNotifications()
+            ->renderHook(
+                PanelsRenderHook::TOPBAR_START,
+                fn(): string => view('filament.admin.topbar-home-button')->render(),
+            )
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\Filament\Widgets')
             ->widgets([
                 AccountWidget::class,
---
+++ resources/views/filament/admin/topbar-home-button.blade.php
@@ -0,0 +1,10 @@
+<div class="ms-auto flex items-center">
+    <x-filament::button
+        :href="url('/')"
+        tag="a"
+        color="gray"
+        icon="heroicon-o-home"
+    >
+        Halaman Index
+    </x-filament::button>
+</div>
